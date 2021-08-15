e13024f43bdad8d18c1f3373dda4e2b9ecc7c517
Rami Hansen <rami.hansen2@gmail.com>
2021-08-15 11:39:59 -0700

    WAV_t method prototypes and definitions

--- include/WAVparser.h
+++ include/WAVparser.h
@@ -1,15 +1,59 @@
 #include <string>
+#include <cstdint>
+#include <vector>
 
 #include "RIFFparser.h"
 
 #pragma once
 
+#pragma pack(2)
+struct WAV_fmt_t
+{
+    uint16_t audio_format{1};
+    uint16_t num_channels{2};
+    uint32_t sample_rate{44100};
+    uint32_t byte_rate{176400};
+    uint16_t block_align{4};
+    uint16_t bits_per_sample{16};
+    uint16_t extra_params_size{0};
+    std::vector<uint8_t> extra_params; // generally don't exist
+};
+
 class WAV_t
 {
 private:
-    RIFF_t riff;
+    RIFF_t m_riff;
+
+    // quick access
+    RIFF_chunk_data_t *m_data();
+    RIFF_chunk_data_t *m_fmt();
+
+    int write_fmt();
+    int write_data();
 
 public:
+    WAV_fmt_t header;
+    std::vector<uint64_t> data;
+
     WAV_t();
     WAV_t(std::string filename);
+
+    void load_fmt();
+    void load_data();
+
+    std::vector<uint8_t> &get_fmt();
+    std::vector<uint8_t> &get_data();
+    RIFF_t &get_riff();
+
+    int sample_size();
+    int get_sample(int i, int channel = 0);
+
+    uint32_t calculate_byte_rate();
+    uint16_t calculate_block_align();
+
+    void clear_data();
+
+    void set_file_path(std::string new_file_path);
+
+    int write();
 };
--- src/WAVparser.cpp
+++ src/WAVparser.cpp
@@ -4,6 +4,62 @@ WAV_t::WAV_t()
 {
 }
 
-WAV_t::WAV_t(std::string filename)
+WAV_t::WAV_t(std::string filename) : m_riff(filename)
+{
+}
+
+int WAV_t::write_fmt()
+{
+}
+
+int WAV_t::write_data()
+{
+}
+
+void WAV_t::load_fmt()
+{
+}
+
+void WAV_t::load_data()
+{
+}
+
+std::vector<uint8_t> &WAV_t::get_fmt()
+{
+}
+
+std::vector<uint8_t> &WAV_t::get_data()
+{
+}
+
+RIFF_t &WAV_t::get_riff()
+{
+}
+
+int WAV_t::sample_size()
+{
+}
+
+int WAV_t::get_sample(int i, int channel)
+{
+}
+
+uint32_t WAV_t::calculate_byte_rate()
+{
+}
+
+uint16_t WAV_t::calculate_block_align()
+{
+}
+
+void WAV_t::clear_data()
+{
+}
+
+void WAV_t::set_file_path(std::string new_file_path)
+{
+}
+
+int WAV_t::write()
 {
 }
